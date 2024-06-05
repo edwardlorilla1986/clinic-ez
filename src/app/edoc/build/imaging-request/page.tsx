bb456a476aef9276dcbc082e33e048ebf37c6cbf
Edward Lance Lorilla <edwardlance.lorilla@ustp.edu.ph>
2024-06-05 18:08:58 +0800

    update initial

--- src/app/edoc/build/imaging-request/page.tsx
+++ src/app/edoc/build/imaging-request/page.tsx
@@ -2,7 +2,7 @@
 import FormBuilderProvider from '@/src/context/FormBuilderContext';
 import React from 'react';
 import Prescription from "@/src/components/Edocs/Build/Prescription";
-import MedicalCertificate from "@/src/components/Edocs/Build/MedicalCertificate";
+import MedicalCertificate from "@/src/components/Edocs/Build/LaboratoryRequest";
 import ImagingRequest from "@/src/components/Edocs/Build/ImagingRequest";
 
 
--- src/app/edoc/generate/laboratory-request/page.tsx
+++ src/app/edoc/generate/laboratory-request/page.tsx
@@ -1,7 +1,7 @@
 
 import FormBuilderProvider from '@/src/context/FormBuilderContext';
 import React from 'react';
-import MedicalCertificate from "@/src/components/Edocs/Generate/MedicalCertificate";
+import MedicalCertificate from "@/src/components/Edocs/Generate/LaboratoryRequest";
 
 
 
